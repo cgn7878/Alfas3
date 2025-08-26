0ab628aae7255b6add01f574c611ab4845812115
cgn7878 <cagancifci@yandex.com>
2025-08-26 15:06:49 +0300

    Create config.py

---
+++ config.py
@@ -0,0 +1,13 @@
+# config.py
+
+TELEGRAM_TOKEN = "7981029346:AAHzDEJtHmZt8V0ZEt7JoT59stlKn1_4DKk"
+TELEGRAM_CHAT_ID = "7831759991"
+NEWS_API_KEY = "db2190b22dd8413c8a326590a1dc9fe1"
+
+# Varsayılan olarak takip edilecek en bilindik 20 coin
+DEFAULT_COINS = [
+    "bitcoin", "ethereum", "binancecoin", "ripple", "cardano",
+    "dogecoin", "solana", "polkadot", "tron", "avalanche-2",
+    "polygon", "chainlink", "litecoin", "stellar", "uniswap",
+    "cosmos", "internet-computer", "monero", "bitcoin-cash", "aptos"
+]
